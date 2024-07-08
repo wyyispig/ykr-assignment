5affe39d3b386c04237880d602ee277d9bdc5c81
zhangzeqing01 <zhangzeqing01@baidu.com>
2024-07-08 16:22:00 +0800

    后端STT和LM逻辑的实现

---
+++ back_end/config.py
@@ -0,0 +1,9 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class Config:
+    api_key = "9fcb46573d2c9f6eb36bb83f02cf7279"
+    api_secret = "YWI4NjgwZThlMTc4M2EzMmM2NjY4MDJh"
+    app_id = "bf22d0cb"
+    secret_key = "c669d52453ae8c880db6a566e59ee5b6"
